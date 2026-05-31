07e154f4326a7626022c8656132e0ebdd15e417b
JasonGit805 <campbellj900@gmail.com>
2026-05-31 13:36:15 -0700

    Add files via upload

---
+++ daily_weather_santa_clara.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import requests
+import sqlite3
+import os
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=37.3541&longitude=-121.9552&hourly=temperature_2m,precipitation_probability,precipitation,relative_humidity_2m,weather_code"
+
+response = requests.get(url)
+data = response.json()
+
+df = pd.DataFrame(data["hourly"])
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+db_path = os.path.join(BASE_DIR, "data", "weather_data.db")
+conn = sqlite3.connect(db_path)
+#conn = sqlite3.connect(r'C:/Users/campb/Documents/Etl Projects/sc_project/data/weather_data.db')
+
+df.to_sql("weather", conn, if_exists="replace", index=False)
+
+conn.close()
